66a97b19b5eaf2ce19feff60131f1aac9763f331
hellh4mmer <draimaan@moh.gov.my>
2025-06-23 22:20:58 +0800

    feat: add vite config for client

---
+++ client/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: '../public',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3003,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
