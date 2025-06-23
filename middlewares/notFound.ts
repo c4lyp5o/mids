11efdf962c0b12e7b634e8a98146f2b59230a946
hellh4mmer <draimaan@moh.gov.my>
2025-06-23 22:12:53 +0800

    feat; add not found middleware

---
+++ middlewares/notFound.ts
@@ -0,0 +1,7 @@
+import type { Request, Response, NextFunction } from 'express';
+
+const notFound = (_req: Request, res: Response, _next: NextFunction) => {
+  res.status(404).json({ message: 'Not Found' });
+};
+
+export default notFound;
