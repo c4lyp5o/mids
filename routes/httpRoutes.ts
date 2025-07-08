bb06e249597aa7be9ba46502e969c2b339f403b5
c4lyp5o <calypso@calypsocloud.one>
2025-07-08 11:09:42 +0800

    fix: specify function return

--- routes/httpRoutes.ts
+++ routes/httpRoutes.ts
@@ -1,71 +1,95 @@
-import { Router } from 'express';
-import type { Request, Response, NextFunction } from 'express';
+import { Router } from "express";
+import type { Request, Response, NextFunction } from "express";
 import {
-  healthCheck,
-  addMosqueAccount,
-  getMosqueAccount,
-  updateMosqueAccount,
-  deleteMosqueAccount,
-  uploadImages,
-  getMosqueImages,
-  deleteImage,
-} from '../controllers/httpControllers';
-import uploadSystem from '../middlewares/multer';
-import { jwtAuth } from '../middlewares/jwtAuth';
+	healthCheck,
+	addMosqueAccount,
+	getMosqueAccount,
+	updateMosqueAccount,
+	deleteMosqueAccount,
+	uploadImages,
+	getMosqueImages,
+	deleteImage,
+} from "../controllers/httpControllers";
+import uploadSystem from "../middlewares/multer";
+import { jwtAuth } from "../middlewares/jwtAuth";
 
 const router = Router();
 
 router.get(
-  '/healthcheck',
-  healthCheck as (req: Request, res: Response, next: NextFunction) => any
+	"/healthcheck",
+	healthCheck as (
+		req: Request,
+		res: Response,
+		next: NextFunction,
+	) => Promise<void>,
 );
 router
-  .route('/')
-  .post(
-    addMosqueAccount as (req: Request, res: Response, next: NextFunction) => any
-  );
+	.route("/")
+	.post(
+		addMosqueAccount as (
+			req: Request,
+			res: Response,
+			next: NextFunction,
+		) => Promise<void>,
+	);
 
 router
-  .route('/:id')
-  .get(
-    jwtAuth as (req: Request, res: Response, next: NextFunction) => any,
-    getMosqueAccount as (req: Request, res: Response, next: NextFunction) => any
-  )
-  .put(
-    jwtAuth as (req: Request, res: Response, next: NextFunction) => any,
-    updateMosqueAccount as (
-      req: Request,
-      res: Response,
-      next: NextFunction
-    ) => any
-  )
-  .delete(
-    jwtAuth as (req: Request, res: Response, next: NextFunction) => any,
-    deleteMosqueAccount as (
-      req: Request,
-      res: Response,
-      next: NextFunction
-    ) => any
-  );
+	.route("/:id")
+	.get(
+		jwtAuth as (req: Request, res: Response, next: NextFunction) => void,
+		getMosqueAccount as (
+			req: Request,
+			res: Response,
+			next: NextFunction,
+		) => Promise<void>,
+	)
+	.put(
+		jwtAuth as (req: Request, res: Response, next: NextFunction) => void,
+		updateMosqueAccount as (
+			req: Request,
+			res: Response,
+			next: NextFunction,
+		) => Promise<void>,
+	)
+	.delete(
+		jwtAuth as (req: Request, res: Response, next: NextFunction) => void,
+		deleteMosqueAccount as (
+			req: Request,
+			res: Response,
+			next: NextFunction,
+		) => Promise<void>,
+	);
 
 // Image-specific routes
 router
-  .route('/:id/images')
-  .get(
-    jwtAuth as (req: Request, res: Response, next: NextFunction) => any,
-    getMosqueImages as (req: Request, res: Response, next: NextFunction) => any
-  )
-  .post(
-    jwtAuth as (req: Request, res: Response, next: NextFunction) => any,
-    uploadSystem,
-    uploadImages as (req: Request, res: Response, next: NextFunction) => any
-  );
+	.route("/:id/images")
+	.get(
+		jwtAuth as (req: Request, res: Response, next: NextFunction) => void,
+		getMosqueImages as (
+			req: Request,
+			res: Response,
+			next: NextFunction,
+		) => Promise<void>,
+	)
+	.post(
+		jwtAuth as (req: Request, res: Response, next: NextFunction) => void,
+		uploadSystem,
+		uploadImages as (
+			req: Request,
+			res: Response,
+			next: NextFunction,
+		) => Promise<void>,
+	);
 
 router
-  .route('/:id/images/:filename')
-  .delete(
-    jwtAuth as (req: Request, res: Response, next: NextFunction) => any,
-    deleteImage as (req: Request, res: Response, next: NextFunction) => any
-  );
+	.route("/:id/images/:filename")
+	.delete(
+		jwtAuth as (req: Request, res: Response, next: NextFunction) => void,
+		deleteImage as (
+			req: Request,
+			res: Response,
+			next: NextFunction,
+		) => Promise<void>,
+	);
 
 export default router;
